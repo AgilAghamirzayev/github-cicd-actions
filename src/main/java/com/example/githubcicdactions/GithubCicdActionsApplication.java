13d3175c7bf853f667814a88a60ead0ea2b18bd9
Aqil <aqilzeka99@gmail.com>
2021-11-14 19:56:48 +0400

    Initial commit

---
+++ src/main/java/com/example/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,19 @@
+package com.example.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class GithubCicdActionsApplication {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Aqil";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
+
+}
